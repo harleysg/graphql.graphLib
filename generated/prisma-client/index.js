ecbe1c2582f9076f2cecda6b6fd4122fe00ef0c9
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-04-19 19:51:52 -0500

    feat(*) Se generan archivos de Prisma

---
+++ generated/prisma-client/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Book",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/harley-santos-garzon-173e12/learn-graphql/dev`
+});
+exports.prisma = new exports.Prisma();
