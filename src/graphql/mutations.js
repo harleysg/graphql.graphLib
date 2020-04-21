37578a0ba7478ce2250dfd40822f6eed5b546d88
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-04-20 21:28:41 -0500

    feat(mutations.js): se configura función createBook para consultar base de datos en prisma

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,6 +1,19 @@
 const Mutation = {
-	createBook(parent, args, context, info) {
-		return { title: "don't exist book" };
+	async createBook(parent, args, context, info) {
+		const book = await context.db.mutation.createBook(
+			{
+				data: {
+					...args /* Se recomiendo no usarlo asi, mejor validar los argumentos antes de enviarlos a la base de datos*/,
+				},
+			},
+			info
+		);
+
+		// TODO: Send email notificatino
+		// TODO: Make a post
+		// TODO: Send user notification
+
+		return book;
 	},
 };
 
